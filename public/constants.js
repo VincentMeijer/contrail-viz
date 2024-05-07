1ec4ef700d5b829d4944c222e6e65f55c7153d09
VincentMeijer <vrmeijer@tudelft.nl>
2024-05-07 23:33:43 +0200

    Move fuel data to separate file

---
+++ public/constants.js
@@ -0,0 +1,3 @@
+export const fuelData = {"Jet-A" : {"eta" : 0.4, "EI_H2O" : 1.23, "LHV" : 42e6},
+                        "SAF" : {"eta" : 0.4, "EI_H2O" : 1.371, "LHV" : 44.17e6},
+                        "Hydrogen" : {"eta" : 0.4, "EI_H2O" : 9.0, "LHV" : 120.9e6}};
